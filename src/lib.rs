e671d5e373e39fd8ca9d91047bba2200f2d607fa
Takeru Ohta <phjgt308@gmail.com>
2022-03-20 22:26:31 +0900

    Add validation code

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("`desired_output_level` must be a finite positive number, but got {value}")]
+    InvalidDesiredOutputLevel { value: f32 },
+
+    #[error("`distortion_factor` must be a number within `0.0 ..= 1.0`, but got {value}")]
+    InvalidDistortionFactor { value: f32 },
+}
+
 #[derive(Debug)]
 pub struct MonoAgc {
     desired_output_level: f32,
@@ -7,14 +16,24 @@ pub struct MonoAgc {
 }
 
 impl MonoAgc {
-    pub fn new(desired_output_level: f32, distortion_factor: f32) -> Self {
-        // TODO: validate
-        Self {
+    pub fn new(desired_output_level: f32, distortion_factor: f32) -> Result<Self, Error> {
+        if !(desired_output_level > 0.0 && desired_output_level.is_finite()) {
+            return Err(Error::InvalidDesiredOutputLevel {
+                value: desired_output_level,
+            });
+        }
+        if !(0.0..=1.0).contains(&distortion_factor) {
+            return Err(Error::InvalidDistortionFactor {
+                value: distortion_factor,
+            });
+        }
+
+        Ok(Self {
             desired_output_level,
             distortion_factor,
             gain: 1.0,
             freezed: false,
-        }
+        })
     }
 
     pub fn freeze_gain(&mut self, freeze: bool) {
