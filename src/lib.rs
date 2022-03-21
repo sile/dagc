ace36c7531ff416c39db02f91f72becc605ea58e
Takeru Ohta <phjgt308@gmail.com>
2022-03-21 17:03:03 +0900

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+//! An implementation of digital AGC based on the following paper:
+//! - [Design and implementation of a new digital automatic gain control](https://hal.univ-lorraine.fr/hal-01397371/document)
+//!
+//! Demo: <https://sile.github.io/dagc/examples/agc.html>
+#![warn(missing_docs)]
+
+/// Possible errors.
 #[derive(Debug, thiserror::Error)]
+#[allow(missing_docs)]
 pub enum Error {
     #[error("`desired_output_rms` must be a finite positive number, but got {value}")]
     InvalidDesiredOutputRms { value: f32 },
@@ -7,6 +15,7 @@ pub enum Error {
     InvalidDistortionFactor { value: f32 },
 }
 
+/// AGC for monaural channel.
 #[derive(Debug)]
 pub struct MonoAgc {
     desired_output_rms: f32,
@@ -16,6 +25,13 @@ pub struct MonoAgc {
 }
 
 impl MonoAgc {
+    /// Makes a new [`MonoAgc`] instance.
+    ///
+    /// `desired_output_rms` specifies the target volume in terms of RMS.
+    ///
+    /// `distortion factor` specifies a factor that determines how quickly or radically changes the gain value.
+    /// If this value is too large, the AGC processing could introduce distortion to the output signals
+    /// (usucally values such as `0.001` or `0.0001` are appropriate).
     pub fn new(desired_output_rms: f32, distortion_factor: f32) -> Result<Self, Error> {
         if !(desired_output_rms > 0.0 && desired_output_rms.is_finite()) {
             return Err(Error::InvalidDesiredOutputRms {
@@ -36,18 +52,27 @@ impl MonoAgc {
         })
     }
 
+    /// If `freeze` is `true`, [`MonoAgc::process()`] becomes not to update the gain value.
+    ///
+    /// For example, if you apply AGC to an audio stream including speech, it would be preferable to freeze the gain update during the non-speech part to prevent amplifying background noises.
+    ///
     pub fn freeze_gain(&mut self, freeze: bool) {
         self.freezed = freeze;
     }
 
-    pub const fn is_gain_freezed(&self) -> bool {
+    /// Returns whether the gain update is frozen or not.
+    pub const fn is_gain_frozen(&self) -> bool {
         self.freezed
     }
 
+    /// Returns the current gain value.
     pub const fn gain(&self) -> f32 {
         self.gain
     }
 
+    /// Applies AGC to an input frame.
+    ///
+    /// If [`MonoAgc::is_gain_frozen()`] is `false`, the gain value is also updated during the processing.
     pub fn process(&mut self, samples: &mut [f32]) {
         for x in samples {
             *x *= self.gain;
